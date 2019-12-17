02ea7eb2f858615fbd710d15e2fd2e2d6d1ce6bf
Andrei Ursta <andrei_ursta@dell.com>
2019-12-17 12:03:53 +0200

    Add reducer toggle

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -2,3 +2,4 @@ export const ADD_TODO = 'ADD_TODO';
 export const REMOVE_TODO = 'REMOVE_TODO';
 export const SELECT_TODO = 'SELECT_TODO';
 export const UPDATE_TODO = 'UPDATE_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
--- src/reducers/toDoReducer.js
+++ src/reducers/toDoReducer.js
@@ -10,44 +10,50 @@ const initialToDos = [
     {
         id: 1,
         content: "TODO 2",
-        checked: false
+        checked: true
     }];
 
 const todos = (state = initialToDos, action) => {
     console.log(action);
-    if(action.type === actionTypes.ADD_TODO)
-    {
-        return  [...state, action.payload];
+    if (action.type === actionTypes.ADD_TODO) {
+        return [...state, action.payload];
     }
-    if(action.type === actionTypes.REMOVE_TODO)
-    {
-        const arr = state.filter((item) => { return item.id !== action.payload.id});
+    if (action.type === actionTypes.REMOVE_TODO) {
+        const arr = state.filter((item) => { return item.id !== action.payload.id });
         return arr;
     }
 
-    if(action.type === actionTypes.UPDATE_TODO){
+    if (action.type === actionTypes.UPDATE_TODO) {
         return state.map((todo) => {
             if (todo.id !== action.payload.id) {
-              return todo
+                return todo
             }
-        
+
             return {
-              ...todo,
-              ...action.payload
+                ...todo,
+                ...action.payload
             }
-          })
+        })
+    }
+
+    if (action.type === actionTypes.TOGGLE_TODO) {
+        return state.map((todo) => {
+            if (todo.id !== action.payload.id) {
+                return todo
+            }
+            return { ...todo, checked: !todo.checked }
+        })
     }
 
     return state;
 }
 
 const selectedToDo = (state = initialSelectedToDo, action) => {
-    if(action.type === actionTypes.SELECT_TODO)
-    {
+    if (action.type === actionTypes.SELECT_TODO) {
         return action.payload;
     }
 
     return state;
 }
 
-export default {todos,selectedToDo} ;
+export default { todos, selectedToDo };
