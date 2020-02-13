9b3bc40787ed4b33e8e855e73e46055c238ef84b
Andrei Ursta <Andrei_Ursta@Dell.com>
2020-02-13 16:30:06 +0200

    Add redux storage for localstorage persistence

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,52 +1,69 @@
-import * as actionTypes from './actionTypes';
-import { getToDos, postToDo, deleteToDo, patchToDo } from '../apis/api';
+import * as actionTypes from "./actionTypes";
+import store, { storageLoader } from "../store";
 
-export const addToDo =(text) => async (dispatch)=>{
-    let newToDo = {
-        content: text,
-        checked: false
-    };
-    let response = await postToDo(newToDo);
-    dispatch ({
-        type : actionTypes.ADD_TODO,
-        payload: response.data
-    });
-}
+import { getToDos, postToDo, deleteToDo, patchToDo } from "../apis/api";
 
-export const removeToDo = (id) => async (dispatch)=>{
-    await deleteToDo(id);
-    dispatch( {
-        type: actionTypes.REMOVE_TODO,
-        payload: {id}
-    });
-}
+export const addToDo = text => async dispatch => {
+  let newToDo = {
+    content: text,
+    checked: false
+  };
+  let response = await postToDo(newToDo);
+  dispatch({
+    type: actionTypes.ADD_TODO,
+    payload: response.data
+  });
+};
 
-export const selectToDo = (id) => {
-    return {
-        type: actionTypes.SELECT_TODO,
-        payload :  id 
-    }
-}
+export const removeToDo = id => async dispatch => {
+  await deleteToDo(id);
+  dispatch({
+    type: actionTypes.REMOVE_TODO,
+    payload: { id }
+  });
+};
 
-export const updateToDo = (toDo) => async (dispatch) =>{
-    let response = await patchToDo(toDo);
-    dispatch( {
-        type: actionTypes.UPDATE_TODO,
-        payload: {...toDo, ...response.data}
-    });
-}
+export const selectToDo = id => {
+  return {
+    type: actionTypes.SELECT_TODO,
+    payload: id
+  };
+};
 
-export const toggleToDo = (id) => {
-    return {
-        type: actionTypes.TOGGLE_TODO,
-        payload :  id 
-    }
-}
+export const updateToDo = toDo => async dispatch => {
+  let response = await patchToDo(toDo);
+  dispatch({
+    type: actionTypes.UPDATE_TODO,
+    payload: { ...toDo, ...response.data }
+  });
+};
 
-export const getToDoCollection= () => async (dispatch)=>{
-    let response = await getToDos();
+export const toggleToDo = id => {
+  return {
+    type: actionTypes.TOGGLE_TODO,
+    payload: id
+  };
+};
+
+// export const getToDoCollection= () => async (dispatch)=>{
+//     let response = await getToDos();
+//     dispatch({
+//         type:actionTypes.GET_TODO_COLLECTION,
+//         payload:[...response.data]
+//     });
+// }
+
+export const getToDoCollection = () => async dispatch => {
+  try {
+    let newState = await storageLoader(store);
     dispatch({
-        type:actionTypes.GET_TODO_COLLECTION,
-        payload:[...response.data]
+      type: actionTypes.GET_TODO_COLLECTION,
+      payload: newState.todos
     });
-}
+  } catch {
+    dispatch({
+      type: actionTypes.GET_TODO_COLLECTION,
+      payload: []
+    });
+  }
+};
--- src/store.js
+++ src/store.js
@@ -1,10 +1,33 @@
 import { combineReducers } from "redux";
 import todos from "./reducers/toDoReducer";
 
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import * as storage from "redux-storage";
+import createEngine from "redux-storage-engine-localstorage";
 
-const reducer = combineReducers(todos);
+// We need to wrap the base reducer, as this is the place where the loaded
+// state will be injected.
+//
+// Note: The reducer does nothing special! It just listens for the LOAD
+//       action and merge in the provided state :)
+// Note: A custom merger function can be passed as second argument
+const reducer = storage.reducer(combineReducers(todos));
+
+// Now it's time to decide which storage engine should be used
+//
+// Note: The arguments to `createEngine` are different for every engine!
+const engine = createEngine("my-save-key");
+
+// And with the engine we can create our middleware function. The middleware
+// is responsible for calling `engine.save` with the current state afer
+// every dispatched action.
+//
+// Note: You can provide a list of action types as second argument, those
+//       actions will be filtered and WON'T trigger calls to `engine.save`!
+const storageMiddleware = storage.createMiddleware(engine, ["SELECT_TODO"]);
+export const storageLoader = storage.createLoader(engine);
 
 export default configureStore({
-  reducer
+  reducer,
+  middleware: [...getDefaultMiddleware(), storageMiddleware]
 });
