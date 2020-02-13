5a64794a4235e7326551f5714d0195f64c457999
Andrei Ursta <Andrei_Ursta@Dell.com>
2020-02-13 15:56:31 +0200

    used configure store from RTK

--- src/store.js
+++ src/store.js
@@ -1,6 +1,10 @@
-import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
-import todos from './reducers/toDoReducer';
-import thunk from 'redux-thunk';
+import { combineReducers } from "redux";
+import todos from "./reducers/toDoReducer";
 
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-export default createStore(combineReducers(todos), composeEnhancers(applyMiddleware(thunk)));
+import { configureStore } from "@reduxjs/toolkit";
+
+const reducer = combineReducers(todos);
+
+export default configureStore({
+  reducer
+});
